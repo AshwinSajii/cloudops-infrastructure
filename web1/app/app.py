e3d761a8af3aaddd3c1f34b47b47ad6ae357eb78
Ashwin Saji <ashwinsaji017@gmail.com>
2025-12-12 13:02:12 +0000

    Added simple CI pipeline

--- web1/app/app.py
+++ web1/app/app.py
@@ -5,10 +5,10 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-S3_ENDPOINT = "http://minio:9000"
-S3_BUCKET = "uploads"
-S3_ACCESS = "minioadmin"
-S3_SECRET = "minioadmin"
+S3_ENDPOINT = os.getenv("S3_ENDPOINT")
+S3_BUCKET   = os.getenv("S3_BUCKET")
+S3_ACCESS   = os.getenv("S3_ACCESS_KEY")
+S3_SECRET   = os.getenv("S3_SECRET_KEY")
 
 s3 = boto3.client(
     "s3",
@@ -17,12 +17,10 @@ s3 = boto3.client(
     aws_secret_access_key=S3_SECRET
 )
 
-# HOME
 @app.route("/")
 def home():
     return redirect(url_for("gallery"))
 
-# UPLOAD PAGE
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
@@ -32,17 +30,15 @@ def upload():
             return redirect(url_for("gallery"))
     return render_template("upload.html")
 
-# GALLERY PAGE
 @app.route("/gallery")
 def gallery():
     objects = s3.list_objects_v2(Bucket=S3_BUCKET).get("Contents", [])
     return render_template("gallery.html", objects=objects)
 
-# DOWNLOAD FILE
 @app.route("/download/<filename>")
 def download(filename):
     obj = s3.get_object(Bucket=S3_BUCKET, Key=filename)
     return send_file(BytesIO(obj['Body'].read()), as_attachment=True, download_name=filename)
-    
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
--- web2/app/app.py
+++ web2/app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
 import boto3
-import os
 from io import BytesIO
 
 app = Flask(__name__)
 
+# Correct MinIO connection settings
 S3_ENDPOINT = "http://minio:9000"
 S3_BUCKET = "uploads"
 S3_ACCESS = "minioadmin"
@@ -17,12 +17,10 @@ s3 = boto3.client(
     aws_secret_access_key=S3_SECRET
 )
 
-# HOME
 @app.route("/")
 def home():
     return redirect(url_for("gallery"))
 
-# UPLOAD PAGE
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
@@ -32,17 +30,15 @@ def upload():
             return redirect(url_for("gallery"))
     return render_template("upload.html")
 
-# GALLERY PAGE
 @app.route("/gallery")
 def gallery():
     objects = s3.list_objects_v2(Bucket=S3_BUCKET).get("Contents", [])
     return render_template("gallery.html", objects=objects)
 
-# DOWNLOAD FILE
 @app.route("/download/<filename>")
 def download(filename):
     obj = s3.get_object(Bucket=S3_BUCKET, Key=filename)
     return send_file(BytesIO(obj['Body'].read()), as_attachment=True, download_name=filename)
-    
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
