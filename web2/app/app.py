61cef7d6e40d2976c7d984ef27e9460055216af1
root <root@LAPTOP-OBV5HS52.localdomain>
2025-12-12 11:48:43 +0000

    initial cloudops commit

---
+++ web2/app/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file
+import boto3
+import os
+from io import BytesIO
+
+app = Flask(__name__)
+
+S3_ENDPOINT = "http://minio:9000"
+S3_BUCKET = "uploads"
+S3_ACCESS = "minioadmin"
+S3_SECRET = "minioadmin"
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=S3_ENDPOINT,
+    aws_access_key_id=S3_ACCESS,
+    aws_secret_access_key=S3_SECRET
+)
+
+# HOME
+@app.route("/")
+def home():
+    return redirect(url_for("gallery"))
+
+# UPLOAD PAGE
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        if file:
+            s3.upload_fileobj(file, S3_BUCKET, file.filename)
+            return redirect(url_for("gallery"))
+    return render_template("upload.html")
+
+# GALLERY PAGE
+@app.route("/gallery")
+def gallery():
+    objects = s3.list_objects_v2(Bucket=S3_BUCKET).get("Contents", [])
+    return render_template("gallery.html", objects=objects)
+
+# DOWNLOAD FILE
+@app.route("/download/<filename>")
+def download(filename):
+    obj = s3.get_object(Bucket=S3_BUCKET, Key=filename)
+    return send_file(BytesIO(obj['Body'].read()), as_attachment=True, download_name=filename)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
